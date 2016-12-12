8689c71798bc45489486c0a9488ea279055a018b
Li Yan <li.yan.ly414@gmail.com>
2016-12-12 10:52:25 +0100

    Keras on Hebbe, avereaging more runs helps

--- keras/Bag/processResults.py
+++ keras/Bag/processResults.py
@@ -32,20 +32,20 @@ w0 = 1.0/ndim*np.ones((ndim, ))
 _, y_train, _, _ = process_data_keras(nfolds=5, nrows=None, random_state=0)
 y_train = pd.DataFrame(y_train)
 
-#w0 = np.ones((y_train_pred.shape[1], ))/y_train_pred.shape[1]
-#obj_geo = partial(obj_opt_geo, y_true=y_train, y_pred=y_train_pred)
-#y_test_geo, w_geo, res_geo = optimize_weights(obj_geo, w0, y_train_pred, y_test_pred, y_train)
+w0 = np.ones((y_train_pred.shape[1], ))/y_train_pred.shape[1]
+obj_geo = partial(obj_opt_geo, y_true=y_train, y_pred=y_train_pred)
+y_test_geo, w_geo, res_geo = optimize_weights(obj_geo, w0, y_train_pred, y_test_pred, y_train)
 #
 #obj_lin = partial(obj_opt_lin, y_true=y_train, y_pred=y_train_pred)
 #y_test_lin, w_lin, res_lin = optimize_weights(obj_lin, w0, y_train_pred, y_test_pred, y_train)
 
-xgbreg = xgb.XGBRegressor(max_depth=6, min_child_weight=1, learning_rate=0.1,
-                          n_estimators=2000, objective=logregobj2, 
-                          base_score=7.8, reg_alpha=1, gamma=1, subsample=0.4,
-                          colsample_bytree=0.5, )
-#xgbreg.fit(y_train_pred, y_train), 
-y_train_pred = pd.DataFrame(y_train_pred)
-y_test_pred = pd.DataFrame(y_test_pred)
-y_test_pred, y_train_pred, mae, ntree = \
-    cv_predict_xgb(xgbreg, y_train_pred, y_train, y_test_pred, cv=5, 
-                   random_state=0, esr=50)
+#xgbreg = xgb.XGBRegressor(max_depth=6, min_child_weight=1, learning_rate=0.1,
+#                          n_estimators=2000, objective=logregobj2, 
+#                          base_score=7.8, reg_alpha=1, gamma=1, subsample=0.4,
+#                          colsample_bytree=0.5, )
+##xgbreg.fit(y_train_pred, y_train), 
+#y_train_pred = pd.DataFrame(y_train_pred)
+#y_test_pred = pd.DataFrame(y_test_pred)
+#y_test_pred, y_train_pred, mae, ntree = \
+#    cv_predict_xgb(xgbreg, y_train_pred, y_train, y_test_pred, cv=5, 
+#                   random_state=0, esr=50)
